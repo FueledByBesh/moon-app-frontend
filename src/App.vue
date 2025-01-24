9ba8073c2ef06cf1b989cafb2d71acdf4a6f4f67
FueledByBesh <olzhasakimbai@gmail.com>
2025-01-24 18:11:51 +0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Home from './components/Home.vue'
+import HelloWorld from './components/Home.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Home id="home" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <div>I don't no what is it</div>
   </main>
 </template>
 
@@ -21,27 +19,7 @@ import TheWelcome from './components/TheWelcome.vue'
 header {
   line-height: 1.5;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.home {
+  width: 100%;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>This is Home Component</div>
+</template>
+
+<style></style>
